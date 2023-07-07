4f3c4ea35bd8bf81f32c09b8c57c7ea9e2d754b0
hareld10@gmail.com <harel.damari@cs.huji.ac.il>
2023-07-08 00:50:33 +0300

    add mura dataset

--- data_aug/contrastive_learning_dataset.py
+++ data_aug/contrastive_learning_dataset.py
@@ -15,7 +15,7 @@ from PIL import Image
 class MuraDataset(Dataset):
     def __init__(self, path, transform):
         self.dataframe = pd.read_pickle(path)
-        self.dataframe = self.dataframe.sample(frac=0.5).reset_index(drop=True)
+        # self.dataframe = self.dataframe.sample(frac=0.5).reset_index(drop=True)
         self.transform = transform  # Add any additional image transformations here
         print("loaded MURA Dataset", self.dataframe.shape)
 
